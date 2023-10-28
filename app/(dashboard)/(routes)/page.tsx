8a9c802f84ac60a9e1cc3a3a658722eb3dc02b89
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-10-29 01:02:17 +0530

    navbar for Student Completed

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div></div>
-      <UserButton afterSignOutUrl="/" />
+      {/* <UserButton afterSignOutUrl="/" /> */}
     </>
   );
 }
---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export const Logo = () => {
+  return (
+    <>
+      {/* <Image height={50} width={50} alt="logo" src="/logo.svg" />; */}
+      <h1 className="font-bold text-blue-700 text-xl">Bestudent</h1>
+    </>
+  );
+};
